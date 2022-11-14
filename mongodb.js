fc5c90b8bf3586ce0191c23fa98f7d1a4335fc3e
jorge007montoya <116775132+jorge007montoya@users.noreply.github.com>
2022-11-14 18:55:20 -0500

    Update mongodb.js
    actualizacion coomit2

--- mongodb.js
+++ mongodb.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
+
 export const db =  mongoose.connect(process.env.MON_DBURI).then(()=> {
     console.log(" La conexion fue realizada 🚴‍♀️ con exito a la bd:🚀 ")
 }).catch((error) => console.error("Problemas al conectar a la Base de datos de Mongodb🥵",error));
